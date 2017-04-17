0de964085759daf679f056a9795f005d4ba27a79
Vishesh <visheshsingh16@gmail.com>
2017-04-17 15:17:35 -0400

    Created Home, about components and router functionality added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
+import { HomeComponent } from './components/home/home.component';
+
+const appRoutes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'about', component: AboutComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    NavbarComponent,
+    SearchComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes),
     HttpModule
   ],
   providers: [],
